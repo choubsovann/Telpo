9796f33fc53adc5a41ec99cf68a22c8218783093
Sovann Developer <sovanndeveloper@gmail.com>
2022-02-22 13:37:35 +0700

    Change

--- lib/telpo.dart
+++ lib/telpo.dart
@@ -6,6 +6,14 @@ import 'package:flutter/services.dart';
 class Telpo {
   static const MethodChannel _channel = MethodChannel('com.telpo.service');
 
+  static Future connect() async {
+    await await _channel.invokeMethod('CONNECT');
+  }
+
+  static Future disconnect() async {
+    await await _channel.invokeMethod('DISCONNECT');
+  }
+
   static Future printImage(Uint8List image) async {
     await _channel.invokeMethod('ACTION_PRINT_IMAGE', {'bytes': image});
   }
